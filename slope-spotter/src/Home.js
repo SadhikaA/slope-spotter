0897f575af03efa2c21155fb32f84a9492688f89
Sadhika Akula <sadhika.akula@berkeley.edu>
2025-04-15 12:20:34 -0700

    speech button, navigation tasks, back button

--- slope-spotter/src/Home.js
+++ slope-spotter/src/Home.js
@@ -2,12 +2,16 @@ import './App.css';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="App">
       <div className="container">
         <h1>Slope Spotter</h1>
         <p className="subtitle">finding a path, slope by slope</p>
+        <div className="input-section">
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+        </div> 
         <button onClick={() => navigate('/navigation')}>Navigate</button>
     </div>
     </div>
