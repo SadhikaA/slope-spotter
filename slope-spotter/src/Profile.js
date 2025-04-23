85690755ab9ccf1b6f56374737bce5196622c5c9
Sebastian Estrada <Sebastian34@berkeley.edu>
2025-04-23 13:34:46 -0700

    Changed the Styling of the Profile Page

--- slope-spotter/src/Profile.js
+++ slope-spotter/src/Profile.js
@@ -75,7 +75,7 @@ function Profile() {
                 </button>
               </div>
 
-              <button className="sign-out-button">
+              <button className="sign-out-button" onClick={()=> navigate('/')}>
                 Sign Out
               </button>
             </div>
